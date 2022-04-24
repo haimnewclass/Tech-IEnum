13200df6a2f9b6f8ce158c4049b22f06671a5d90
haim hod gilboa <haim@newclass.co.il>
2022-04-24 14:04:28 +0300

    Add project files.

---
+++ Tech-IEnum-Console/Cars.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_IEnum_Console
+{
+    internal class Cars:IEnumerable<Car>, IEnumerator<Car>
+    {
+
+        public IEnumerator<Car> GetEnumerator()
+        {
+            return this;
+        }
+
+
+        private List<Car> cars = new List<Car>();
+
+        private int _idx = -1;
+  
+        public void Add(Car c)
+        {
+            cars.Add(c);
+        }
+
+        public void PrintAll()
+        {
+            foreach (Car c in cars)
+            {
+                Console.WriteLine($"{c.id}.{c.Name}.{c.Description}");
+            }
+        }
+
+        public void Dispose()
+        {
+           Reset();
+        }
+
+        public Car Current
+        {
+            // idx = 0
+            //"subaro","Volvo","Mazda"
+            get
+            {
+                if(cars[_idx].km==0)
+                {
+                    return null;
+                }
+
+                return cars[_idx];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public bool MoveNext()
+        {  //"subaro","Volvo","Mazda"
+            _idx++;
+            if (cars.Count <= _idx)
+            {
+                return false;
+            }else
+            {
+                return true;
+            }
+        }
+
+        public void Reset()
+        {
+            _idx = -1;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+    }
+}
---
+++ Tech-IEnum-Console/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech_IEnum_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "aaa", "bbb", "ccc" };
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            foreach (string s in arr)
+            {
+                Console.WriteLine(s);
+            }
+
+            Cars cars = new Cars();
+            cars.Add(new Car() { id = 1, Name = "Subaro",km=100 });
+            cars.Add(new Car() { id = 2, Name = "Tesla" ,km=200});
+            cars.Add(new Car() { id = 3, Name = "Mazda" ,km=23});
+
+            cars.PrintAll();
+
+            foreach (var item in cars)
+            {
+                Console.WriteLine($"{item.Name}.{item.km}");
+            }
+
+            //System.IO.Directory.GetFiles()
+
+        }
+    }
+}
